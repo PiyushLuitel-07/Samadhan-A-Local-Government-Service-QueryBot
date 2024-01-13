ff4058843c74a4a3bf8621c5c2ae37326132e182
Piyush_07 <luitelpiyush95@gmail.com>
2024-01-13 17:49:30 +0545

    some changes done

--- actions/actions.py
+++ actions/actions.py
@@ -8,20 +8,21 @@
 # This is a simple example for a custom action which utters "Hello World!"
 
 # from typing import Any, Text, Dict, List
-#
+
 # from rasa_sdk import Action, Tracker
 # from rasa_sdk.executor import CollectingDispatcher
-#
-#
+# from rasa_sdk.forms import FormAction
+
+# # this is a form 
 # class ActionHelloWorld(Action):
-#
+
 #     def name(self) -> Text:
 #         return "action_hello_world"
-#
+
 #     def run(self, dispatcher: CollectingDispatcher,
 #             tracker: Tracker,
 #             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
+
 #         dispatcher.utter_message(text="Hello World!")
-#
+
 #         return []
