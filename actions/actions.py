c9ec9bfb5832c1b06afc4dcad8f66ad8aa54b227
prashant72-git <84792438+prashant72-git@users.noreply.github.com>
2024-01-18 16:03:54 +0545

    sucessful change phase 1 citizenship

--- actions/actions.py
+++ actions/actions.py
@@ -1,28 +1,86 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+# # This files contains your custom actions which can be used to run
+# # custom Python code.
+# #
+# # See this guide on how to implement these action:
+# # https://rasa.com/docs/rasa/custom-actions
 
 
-#This is a simple example for a custom action which utters "Hello World!"
 
-from typing import Any, Text, Dict, List
-
+from typing import Text, List, Any, Dict
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.forms import FormAction
+from rasa_sdk.events import SlotSet
 
-# this is a form 
-# class ActionHelloWorld(Action):
-
+# class ActionHandleCitizenshipQuery(Action):
 #     def name(self) -> Text:
-#         return "action_hello_world"
+#         return "action_handle_citizenship_query"
 
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         # Extract entities from the user's message
+#         entities = tracker.latest_message['entities'][0]['value']
 
-#         dispatcher.utter_message(text="Hello World!")y
+#         # Default value for citizenship type
+#         citizenship_type = "unspecified"
 
-#         return []
+#         # Check if there are entities and extract citizenship type
+#         if entities:
+#             for entity in entities:
+#                 if entity['entity'] == 'new citizenship':
+#                     citizenship_type = 'new citizenship'
+#                     # Set the slot value for 'citizenship_type'
+#                     dispatcher.utter_message(template="utter_new_citizenship_eligibility_criteria")
+#                     dispatcher.utter_message(template="utter_new_citizenship_documents_required")
+#                     dispatcher.utter_message(template="utter_new_citizenship_steps")                    
+#                     return [SlotSet("citizenship_type", citizenship_type)]
+#                 elif entity['entity'] == 'copy citizenship':
+#                     citizenship_type = 'copy citizenship'
+#                     # Set the slot value for 'citizenship_type'
+#                     dispatcher.utter_message(template="utter_copy_citizenship_eligibility_criteria")
+#                     dispatcher.utter_message(template="utter_copy_citizenship_documents_required")
+#                     dispatcher.utter_message(template="utter_copy_citizenship_steps")
+#                     return [SlotSet("citizenship_type", citizenship_type)]
+#                 elif entity['entity'] == 'adopted citizenship':
+#                     citizenship_type = 'adopted citizenship'
+#                     # Set the slot value for 'citizenship_type'
+#                     dispatcher.utter_message(template="utter_adopted_citizenship_eligibility_criteria")
+#                     dispatcher.utter_message(template="utter_adopted_citizenship_documents_required")
+#                     dispatcher.utter_message(template="utter_adopted_citizenship_steps")
+#                     return [SlotSet("citizenship_type", citizenship_type)]
+
+
+
+class ActionHandleCitizenshipQuery(Action):
+    def name(self) -> Text:
+        return "action_handle_citizenship_query"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # Extract entities from the user's message
+        entities = tracker.latest_message['entities']
+
+        # Default value for citizenship type
+        citizenship_type = "unspecified"
+
+        # Check if there are entities
+        if entities:
+            entity_values = [entity['value'] for entity in entities]
+            
+            if 'new citizenship' in entity_values:
+                citizenship_type = 'new citizenship'
+                dispatcher.utter_message(template="utter_new_citizenship_eligibility_criteria")
+                dispatcher.utter_message(template="utter_new_citizenship_documents_required")
+                dispatcher.utter_message(template="utter_new_citizenship_steps")
+            elif 'copy citizenship' in entity_values:
+                citizenship_type = 'copy citizenship'
+                dispatcher.utter_message(template="utter_copy_citizenship_eligibility_criteria")
+                dispatcher.utter_message(template="utter_copy_citizenship_documents_required")
+                dispatcher.utter_message(template="utter_copy_citizenship_steps")
+            elif 'adopted citizenship' in entity_values:
+                citizenship_type = 'adopted citizenship'
+                dispatcher.utter_message(template="utter_adopted_citizenship_eligibility_criteria")
+                dispatcher.utter_message(template="utter_adopted_citizenship_documents_required")
+                dispatcher.utter_message(template="utter_adopted_citizenship_steps")
+
+        # Set the slot value for 'citizenship_type'
+        return [SlotSet("citizenship_type", citizenship_type)]
+        
+
