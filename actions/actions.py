29cfe49d72f1ddb643546a1badce9e16fda14e38
prashant72-git <84792438+prashant72-git@users.noreply.github.com>
2024-02-07 20:51:07 +0545

    domain and stories updated

--- actions/actions.py
+++ actions/actions.py
@@ -6,50 +6,50 @@
 
 
 
-from typing import Text, List, Any, Dict
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import SlotSet
+# from typing import Text, List, Any, Dict
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+# from rasa_sdk.events import SlotSet
 
 
-class ActionHandleCitizenshipQuery(Action):
-    def name(self) -> Text:
-        return "action_handle_citizenship_query"
+# class ActionHandleCitizenshipQuery(Action):
+#     def name(self) -> Text:
+#         return "action_handle_citizenship_query"
 
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        # Extract entities from the user's message
-        entity_values = next(tracker.get_latest_entity_values('type'),None)
-        if  entity_values == 'citizenship by descent': 
-                dispatcher.utter_message(response="utter_citizenship_by_descent_eligibility_criteria")
-                dispatcher.utter_message(response="utter_citizenship_by_descent_documents_required")
-                dispatcher.utter_message(response="utter_citizenship_by_descent_steps")
-        elif  entity_values == 'citizenship by birth':
-                    dispatcher.utter_message(response="utter_citizenship_by_birth_eligibility_criteria")
-                    dispatcher.utter_message(response="utter_citizenship_by_birth_documents_required")
-                    dispatcher.utter_message(response="utter_citizenship_by_birth_steps")
-        else:
-                dispatcher.utter_message(response="utter_citizenship")
-        return []
+#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         # Extract entities from the user's message
+#         entity_values = next(tracker.get_latest_entity_values('type'),None)
+#         if  entity_values == 'citizenship by descent': 
+#                 dispatcher.utter_message(response="utter_citizenship_by_descent_eligibility_criteria")
+#                 dispatcher.utter_message(response="utter_citizenship_by_descent_documents_required")
+#                 dispatcher.utter_message(response="utter_citizenship_by_descent_steps")
+#         elif  entity_values == 'citizenship by birth':
+#                     dispatcher.utter_message(response="utter_citizenship_by_birth_eligibility_criteria")
+#                     dispatcher.utter_message(response="utter_citizenship_by_birth_documents_required")
+#                     dispatcher.utter_message(response="utter_citizenship_by_birth_steps")
+#         else:
+#                 dispatcher.utter_message(response="utter_citizenship")
+#         return []
 
  
-class ActionHandleCitizenshipQueryRomanized(Action):
-    def name(self) -> Text:
-        return "action_handle_citizenship_query_romanized"
+# class ActionHandleCitizenshipQueryRomanized(Action):
+#     def name(self) -> Text:
+#         return "action_handle_citizenship_query_romanized"
 
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        # Extract entities from the user's message
-        entity_values = next(tracker.get_latest_entity_values('type'),None)
-        if  entity_values == 'descent': 
-                dispatcher.utter_message(response="utter_citizenship_by_descent_eligibility_romanized")
-                dispatcher.utter_message(response="utter_citizenship_by_descent_documents_romanized")
-                dispatcher.utter_message(response="utter_citizenship_by_descent_steps_romanized")
-        elif  entity_values == 'birth':
-                    dispatcher.utter_message(response="utter_citizenship_by_birth_eligibility_romanized")
-                    dispatcher.utter_message(response="utter_citizenship_by_birth_documents_romanized")
-                    dispatcher.utter_message(response="utter_citizenship_by_birth_steps_romanized")
-        else:
-                dispatcher.utter_message(response="utter_citizenship")
-        return []
+#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         # Extract entities from the user's message
+#         entity_values = next(tracker.get_latest_entity_values('type'),None)
+#         if  entity_values == 'descent': 
+#                 dispatcher.utter_message(response="utter_citizenship_by_descent_eligibility_romanized")
+#                 dispatcher.utter_message(response="utter_citizenship_by_descent_documents_romanized")
+#                 dispatcher.utter_message(response="utter_citizenship_by_descent_steps_romanized")
+#         elif  entity_values == 'birth':
+#                     dispatcher.utter_message(response="utter_citizenship_by_birth_eligibility_romanized")
+#                     dispatcher.utter_message(response="utter_citizenship_by_birth_documents_romanized")
+#                     dispatcher.utter_message(response="utter_citizenship_by_birth_steps_romanized")
+#         else:
+#                 dispatcher.utter_message(response="utter_citizenship")
+#         return []
 
 
     
